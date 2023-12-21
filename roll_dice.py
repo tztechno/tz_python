d83f81630af5b8d04912c187dbd1a89bfb32860d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-22 03:28:48 +0900

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -1,5 +1,37 @@
+
+###########################
+
+#python roll_dice.py
+
 import random
 def roll_dice():
     return random.randint(1,6)
 result = roll_dice()
 print(result)
+
+###########################
+
+#python roll_dice.py x
+
+import random
+import sys
+
+def roll_dice(x):
+    return random.randint(1, x)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        x = 6  # デフォルト値
+    elif len(sys.argv) == 2:
+        try:
+            x = int(sys.argv[1])
+        except ValueError:
+            print("Error: Please provide a valid integer for x.")
+            sys.exit(1)
+    else:
+        print("Usage: python roll_dice.py [x]")
+        sys.exit(1)
+
+    result = roll_dice(x)
+    print(result)
+
